e5d8f7e535f56f5f85889f45844ce5af1cb80d50
artxksu <106624452+artxksu@users.noreply.github.com>
2023-03-30 12:23:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,166 @@
+$("document").ready(function () {
+    $(".plate1").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/plate1.svg")
+            $(this).attr("src", "img/plate1js.svg");
+        else if ($(this).attr("src") === "img/plate1js.svg")
+            $(this).attr("src", "img/plate1.svg");
+    });
+});
+$("document").ready(function () {
+    $(".plate2").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/plate2.svg")
+            $(this).attr("src", "img/plate2js.svg");
+        else if ($(this).attr("src") === "img/plate2js.svg")
+            $(this).attr("src", "img/plate2.svg");
+    });
+});
+$("document").ready(function () {
+    $(".plate3").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/plate3.svg")
+            $(this).attr("src", "img/plate3js.svg");
+        else if ($(this).attr("src") === "img/plate3js.svg")
+            $(this).attr("src", "img/plate3.svg");
+    });
+});
+
+$(document).ready(function () {
+    $(".ball").click(function () {
+        $(this).hide();
+    });
+
+});
+$("document").ready(function () {
+    $(".stuff1").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/stuff1.svg")
+            $(this).attr("src", "img/stuff1js.svg");
+        else if ($(this).attr("src") === "img/stuff1js.svg")
+            $(this).attr("src", "img/stuff1.svg");
+    });
+});
+$("document").ready(function () {
+    $(".stuff2").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/stuff2.svg")
+            $(this).attr("src", "img/stuff2js.svg");
+        else if ($(this).attr("src") === "img/stuff2js.svg")
+            $(this).attr("src", "img/stuff2.svg");
+    });
+});
+$("document").ready(function () {
+    $(".stuff3").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/stuff3.svg")
+            $(this).attr("src", "img/stuff3js.svg");
+        else if ($(this).attr("src") === "img/stuff3js.svg")
+            $(this).attr("src", "img/stuff3.svg");
+    });
+});
+$("document").ready(function () {
+    $(".stuff4").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/stuff4.svg")
+            $(this).attr("src", "img/stuff4js.svg");
+        else if ($(this).attr("src") === "img/stuff4js.svg")
+            $(this).attr("src", "img/stuff4.svg");
+    });
+});
+$("document").ready(function () {
+    $(".stuff5").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/stuff5.svg")
+            $(this).attr("src", "img/stuff5js.svg");
+        else if ($(this).attr("src") === "img/stuff5js.svg")
+            $(this).attr("src", "img/stuff5.svg");
+    });
+});
+$("document").ready(function () {
+    $(".stuff6").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/stuff6.svg")
+            $(this).attr("src", "img/stuff6js.svg");
+        else if ($(this).attr("src") === "img/stuff6js.svg")
+            $(this).attr("src", "img/stuff6.svg");
+    });
+});
+$("document").ready(function () {
+    $(".stuff7").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/stuff7.svg")
+            $(this).attr("src", "img/stuff7js.svg");
+        else if ($(this).attr("src") === "img/stuff7js.svg")
+            $(this).attr("src", "img/stuff7.svg");
+    });
+});
+$("document").ready(function () {
+    $(".pribor1").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/pribor1.svg")
+            $(this).attr("src", "img/pribor1js.svg");
+    });
+});
+$("document").ready(function () {
+    $(".pribor2").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/pribor2.svg")
+            $(this).attr("src", "img/pribor2js.svg");
+    });
+});
+$("document").ready(function () {
+    $(".pribor3").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/pribor3.svg")
+            $(this).attr("src", "img/pribor3js.svg");
+    });
+});
+$("document").ready(function () {
+    $(".pribor4").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/pribor4.svg")
+            $(this).attr("src", "img/pribor4js.svg");
+    });
+});
+$("document").ready(function () {
+    $(".pribor5").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/pribor5.svg")
+            $(this).attr("src", "img/pribor5js.svg");
+    });
+});
+$("document").ready(function () {
+    $(".pribor6").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/pribor6.svg")
+            $(this).attr("src", "img/pribor6js.svg");
+    });
+});
+$(function () {
+    $("#pribor1").draggable();
+});
+$(function () {
+    $("#pribor2").draggable();
+});
+$(function () {
+    $("#pribor3").draggable();
+});
+$(function () {
+    $("#pribor4").draggable();
+});
+$(function () {
+    $("#pribor5").draggable();
+});
+$(function () {
+    $("#pribor6").draggable();
+});
+$("document").ready(function () {
+    $(".card5").click(function () {
+        console.log('ddd');
+        if ($(this).attr("src") === "img/card5.svg")
+            $(this).attr("src", "img/card5js.svg");
+        else if ($(this).attr("src") === "img/card5js.svg")
+            $(this).attr("src", "img/card5.svg");
+    });
+});
